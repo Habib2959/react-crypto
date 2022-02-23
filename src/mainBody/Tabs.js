eae552b4fdf454a25fb1bea44d06102b9c2c9b54
Habib <71340861+Habib2959@users.noreply.github.com>
2022-02-23 21:23:59 +0600

    Update Tabs.js

--- src/mainBody/Tabs.js
+++ src/mainBody/Tabs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Tab, Tabs } from '@mui/material';
 import CustomTable from './CustomTable';
 import SecondTab from './SecondTab';
+import "./body.css"
 
 
 const CustomTabs = props => {
